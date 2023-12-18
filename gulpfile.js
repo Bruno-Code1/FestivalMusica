8aa6a880d225959b8dcae9aebf1b8975f6194bfb
Bruno Bettio <bruno.web.bpage@gmail.com>
2023-12-18 15:56:06 -0300

    agregando galeria y terminando ultimons detalles

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,77 @@
-const {src, dest, watch} = require("gulp");
+const {src, dest, watch, parallel} = require("gulp");
+
+// CSS
 const sass = require("gulp-sass")(require("sass"));
 const plumber = require('gulp-plumber');
+
+//Imagenes
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
+const cache = require('gulp-cache');
+const avif = require('gulp-avif');
 
 function css(done) {
     src('src/scss/**/*.scss')// Identificar el archivo de SASS
     .pipe(plumber())
     .pipe(sass())// Compilarlo
-    .pipe(dest("build/css"))// Almacenarla en el disco duro
+    .pipe(dest("build/css"));// Almacenarla en el disco duro
 
     done(); // Callback que avisa a gulp cuando llegamos al final
 }
 
+function imagenes(done) {
+    const opciones = {
+        optimizationLevel: 3
+    }
+    src('src/img/**/*.{png,jpg}')
+        .pipe( cache( imagemin(opciones) ) )
+        .pipe( dest('build/img') )
+    done();
+}
+
+function versionWebp(done) {
+    
+    const opciones = {
+        quality: 50
+    };
+
+    src('src/img/**/*.{png,jpg}')
+    .pipe(webp(opciones))
+    .pipe(dest('build/img'));
+
+    done();
+}
+
+function versionAvif(done) {
+    
+    const opciones = {
+        quality: 50
+    };
+
+    src('src/img/**/*.{png,jpg}')
+    .pipe(avif(opciones))
+    .pipe(dest('build/img'));
+
+    done();
+}
+
+function javascript(done) {
+    src('src/js/**/*.js')
+        .pipe(dest('build/js'));
+    
+        done();
+}
+
 function dev(done) {
     watch('src/scss/**/*.scss', css);
+    watch('src/js/**/*', javascript);
 
     done();
 }
 
 exports.css = css;
-exports.dev = dev;
+exports.js = javascript;
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif;
+exports.dev = parallel(imagenes, versionWebp, versionAvif, dev, javascript);
