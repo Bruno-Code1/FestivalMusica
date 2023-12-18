cd2060df454ca0c34042732e995fceef23d9d4ef
Bruno Bettio <bruno.web.bpage@gmail.com>
2023-12-18 18:32:28 -0300

    terminando

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,10 @@ const {src, dest, watch, parallel} = require("gulp");
 // CSS
 const sass = require("gulp-sass")(require("sass"));
 const plumber = require('gulp-plumber');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const postcsss = require('gulp-postcss');
+const sourcemaps = require('gulp-sourcemaps');
 
 //Imagenes
 const imagemin = require('gulp-imagemin');
@@ -10,10 +14,16 @@ const webp = require('gulp-webp');
 const cache = require('gulp-cache');
 const avif = require('gulp-avif');
 
+// Javascript
+const terser = require('gulp-terser-js');
+
 function css(done) {
     src('src/scss/**/*.scss')// Identificar el archivo de SASS
+    .pipe(sourcemaps.init())
     .pipe(plumber())
-    .pipe(sass())// Compilarlo
+    .pipe(sass())
+    .pipe(postcsss([autoprefixer(), cssnano()]))// Compilarlo
+    .pipe(sourcemaps.write('.'))
     .pipe(dest("build/css"));// Almacenarla en el disco duro
 
     done(); // Callback que avisa a gulp cuando llegamos al final
@@ -57,6 +67,9 @@ function versionAvif(done) {
 
 function javascript(done) {
     src('src/js/**/*.js')
+        .pipe(sourcemaps.init())
+        .pipe(terser())
+        .pipe(sourcemaps.write('.'))
         .pipe(dest('build/js'));
     
         done();
--- src/js/app.js
+++ src/js/app.js
@@ -8,6 +8,34 @@ function iniciarApp() {
     scrollNav();
 }
 
+function navegacionFija() {
+    const barra = document.querySelector('.header');
+    const sobreFestival = document.querySelector('.sobre-festival');
+    const body = document.querySelector('body');
+
+    window.addEventListener('scroll', function() {
+        
+        if(sobreFestival.getBoundingClientRect().top < 0) {
+            barra.classList.add('fijo');
+            body.classList.add('body-scroll');
+        } else {
+            barra.classList.remove('fijo');
+            body.classList.remove('body-scroll');
+        }
+    })
+}
+
+function scrollNav() {
+    const enlaces = document.querySelectorAll('.navegacion-principal');
+    enlaces.forEach(enlace => {
+        enlace.addEventListener('click', function(e) {
+            const seccionScroll = e.target.attributes.href.value;
+            const seccion = document.querySelector(seccionScroll);
+            seccion.scrollIntoView({ behavior: "smooth" });
+        });
+    });
+}
+
 function crearGaleria() {
     const galeria = document.querySelector('.galeria-imagenes');
     
